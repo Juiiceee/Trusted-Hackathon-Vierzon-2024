020d729b8521b7a3ca2cc7ba88a88d96c4f30948
RASJoj0 <joela.rasam@gmail.com>
2024-10-06 00:24:37 +0200

    c'est pour toi chou

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    }
-
-  export default nextConfig;
-  
+export default {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/site', // Redirection vers ton dossier "site"
+        permanent: true, // ou false si c'est une redirection temporaire
+      },
+    ];
+  },
+};
