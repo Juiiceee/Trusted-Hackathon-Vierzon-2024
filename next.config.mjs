8bfc8736b691626f886648d1494869f377939441
RASJoj0 <joela.rasam@gmail.com>
2024-10-05 09:05:04 +0200

    add header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,  // Active l'App Router
+    },
+    async redirects() {
+      return [
+        {
+          source: '/home',         // La route d'origine (page d'accueil)
+          destination: '/site', // La route cible (le dossier "site" sera traité comme home)
+          permanent: true,     // Redirection permanente (301)
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ src/app/site/page.tsx
@@ -0,0 +1,9 @@
+// src/app/site/page.tsx
+export default function SitePage() {
+  return (
+    <div>
+      <h1>Bienvenue sur la page /site</h1>
+      <p>Ceci est la page du dossier "site".</p>
+    </div>
+  );
+}
